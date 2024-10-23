e06b1e5c9f627fd27109c4d7c2e0b1b1a4028b4c
Jad AlHamwi <j.hamwi@centroidsol.com>
2024-10-23 23:21:55 +0400

    image upload component updated

--- src/components/ImageUpload/ImageUpload.tsx
+++ src/components/ImageUpload/ImageUpload.tsx
@@ -3,8 +3,10 @@ import { createFileObject, formatAssetPath, randomId } from "@/utils/utils";
 import { faUpload } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useEffect, useRef, useState } from "react";
+import { BarLoader } from "react-spinners";
 import { Button } from "../ui/button";
 import { Card } from "../ui/card";
+import styles from "./ImageUpload.module.scss";
 
 type Props = {
 	pathPrefix: string;
@@ -13,6 +15,7 @@ type Props = {
 };
 
 const ImageUpload = ({ pathPrefix, path, onChange }: Props) => {
+	const [loading, setLoading] = useState(true);
 	const inputRef = useRef<HTMLInputElement>(null);
 
 	const [imageUrl, setImageUrl] = useState<string | null>(null);
@@ -20,16 +23,23 @@ const ImageUpload = ({ pathPrefix, path, onChange }: Props) => {
 	useEffect(() => {
 		if (!path) {
 			setImageUrl(null);
+			setLoading(false);
 			return;
 		}
 		const fullPath = formatAssetPath(path);
-		createFileObject(fullPath).then((file) => {
-			if (file) {
-				const url = URL.createObjectURL(file);
-				setImageUrl(url);
-			}
-		});
+		createFileObject(fullPath)
+			.then((file) => {
+				if (file) {
+					const url = URL.createObjectURL(file);
+					setImageUrl(url);
+				}
+			})
+			.finally(() => {
+				setLoading(false);
+			});
 	}, [path]);
+
+	const [uploading, setUploading] = useState(false);
 
 	const onUpload = async (e: React.ChangeEvent<HTMLInputElement>) => {
 		const formData = new FormData();
@@ -37,23 +47,42 @@ const ImageUpload = ({ pathPrefix, path, onChange }: Props) => {
 		const storePath = path ? path : `${pathPrefix}/${randomId()}.png`;
 		formData.append("image", image);
 		formData.append("path", storePath);
-		axiosInstance.post("/uploads/images", formData);
+		setUploading(true);
+		axiosInstance.post("/uploads/images", formData).then(() => {
+			setUploading(false);
+		});
 		setImageUrl(URL.createObjectURL(image));
 		onChange(storePath);
 	};
 
+	if (loading) {
+		return (
+			<Card
+				style={{ height: 250, width: 250 }}
+				className="m-auto p-1 grid place-items-center relative"
+			></Card>
+		);
+	}
+
 	return (
 		<Card
 			style={{ height: 250, width: 250 }}
-			className="m-auto p-1 grid place-items-center"
+			className="m-auto p-1 grid place-items-center relative"
 			onClick={() => inputRef.current?.click()}
 		>
 			{imageUrl ? (
-				<img
-					src={imageUrl}
-					style={{ height: 200, width: 200 }}
-					className="rounded-lg"
-				/>
+				<>
+					<img
+						src={imageUrl}
+						style={{ height: 200, width: 200, opacity: uploading ? 0.5 : 1 }}
+						className="rounded-lg"
+					/>
+					{uploading && (
+						<div className={styles.progress_bar_container}>
+							<BarLoader color="white" />
+						</div>
+					)}
+				</>
 			) : (
 				<Button variant={"ghost"} className="h-full w-full">
 					<div className="flex flex-row gap-2 items-center justify-center opacity-50">
