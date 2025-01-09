ef6dd2a047e20b7c1ff9cf4067dff655e574da85
Jad AlHamwi <j.hamwi@centroidsol.com>
2025-01-09 23:29:47 +0400

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ const router = createBrowserRouter(
 				<Route path="/admin/offers" element={<Offers />}></Route>
 			</Route>
 			<Route path="/admin" element={<PublicRoute />}>
-				<Route path="login" element={<LoginPage />}></Route>
+				<Route path="/admin/login" element={<LoginPage />}></Route>
 			</Route>
 		</Route>
 	)
